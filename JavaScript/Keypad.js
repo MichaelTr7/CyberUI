c87bddfe0ac9c59ccc0b1529359d83205337d203
MichaelTr7 <mailmichaeltr@gmail.com>
2021-10-21 11:46:21 -0400

    Keypad update

---
+++ JavaScript/Keypad.js
@@ -0,0 +1,18 @@
+
+var Key_Phrase = ["c","y","b","e","r","t","r","u","c","k"];
+var Current_Letter = "c";
+function Keypad_Listening(){
+  Current_Letter = Key_Phrase.pop();
+  console.log(Current_Letter);
+
+  // Listen for CYBERTRUCK characters
+  
+
+}
+
+function Check_Letter(){  
+  
+  
+  
+   
+} 
--- JavaScript/Landing_Page.js
+++ JavaScript/Landing_Page.js
@@ -3,5 +3,5 @@ window.onload = function(){
   // console.log(Get_CSS_Property("#Main_Panel","--Smallest_Dimension"));
   Slide_To_Unlock_Callback();
   Light_Up_Animation_Callbacks();
-
+  Keypad_Listening();
 }
