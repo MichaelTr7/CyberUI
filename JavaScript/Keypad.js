9e7ef9aca52644387749ade1f90b98d9ddf1de2c
MichaelTr7 <mailmichaeltr@gmail.com>
2021-11-10 10:20:49 -0500

    Mobile Configuration Updates

--- JavaScript/Keypad.js
+++ JavaScript/Keypad.js
@@ -21,49 +21,51 @@ function Hot_Key_Pressed(e){
           Character_Index++;
         }
       }else{
-        let Keyboard = document.getElementsByClassName("Keyboard_Keys");
-        let Target_Key = Keyboard[Keyboard.length - 1];
-        Target_Key.style.backgroundColor = "rgba(255,255,255,0.9)";
-        Target_Key.children[0].style.color = "rgba(255,255,255,0.9)";
-        Target_Key.children[0].style.backgroundColor = "rgb(70,70,70)";
-        Highlight_Key(Target_Key);
-        let Prompt_Label = document.getElementById("Prompt");
-        Prompt_Label.classList.add("Prompt_Vanish");
-      
-        let Slide_To_Unlock_Switch = document.getElementById("Slide_Cavity_Background");
-        Slide_To_Unlock_Switch.style.top = "calc(100% - var(--Height) - var(--Bottom_Spacer) + var(--Padding)/2)";
-        Slide_To_Unlock_Switch.style.opacity = "1";
-        let Slider_Switch = document.getElementById("Slide_Cavity");
-        Slider_Switch.style.opacity = "1";
-        setTimeout(function () {
-          Slider_Switch.style.transition = "0s";
-          Slide_To_Unlock_Switch.style.transition = "0s";
-        }, 1000);
-        
-        setTimeout(function () {
-          var Keyboard_Keys = document.getElementsByClassName("Keyboard_Keys");
-          Keyboard_Keys[0].classList.add("Fade_Down_0");
-          Keyboard_Keys[1].classList.add("Fade_Down_1");
-          Keyboard_Keys[2].classList.add("Fade_Down_2");
-          Keyboard_Keys[3].classList.add("Fade_Down_3");
-          Keyboard_Keys[4].classList.add("Fade_Down_4");
-          Keyboard_Keys[5].classList.add("Fade_Down_5");
-          Keyboard_Keys[6].classList.add("Fade_Down_6");
-          Keyboard_Keys[7].classList.add("Fade_Down_7");
-          Keyboard_Keys[8].classList.add("Fade_Down_8");
-          Keyboard_Keys[9].classList.add("Fade_Down_9");
-        }, 50);
-        document.removeEventListener("keydown",Hot_Key_Pressed);
+          Show_Unlock_Slider();
       }
 }
 
 function Highlight_Key(Target_Key){
-  Target_Key.style.backgroundColor = "rgba(255,255,255,0.9)";
+  Target_Key.classList.add("Key_Toggled");
   Target_Key.children[0].style.color = "rgba(255,255,255,0.9)";
   Target_Key.children[0].style.backgroundColor = "rgb(70,70,70)";
 }
 
 
+function Show_Unlock_Slider(){
+  let Keyboard = document.getElementsByClassName("Keyboard_Keys");
+  let Target_Key = Keyboard[Keyboard.length - 1];
+  Target_Key.classList.add("Key_Toggled");
+  Target_Key.children[0].style.color = "rgba(255,255,255,0.9)";
+  Target_Key.children[0].style.backgroundColor = "rgb(70,70,70)";
+  Highlight_Key(Target_Key);
+  let Prompt_Label = document.getElementById("Prompt");
+  Prompt_Label.classList.add("Prompt_Vanish");
 
+  let Slide_To_Unlock_Switch = document.getElementById("Slide_Cavity_Background");
+  Slide_To_Unlock_Switch.style.top = "calc(100% - var(--Height) - var(--Bottom_Spacer) + var(--Padding)/2)";
+  Slide_To_Unlock_Switch.style.opacity = "1";
+  let Slider_Switch = document.getElementById("Slide_Cavity");
+  Slider_Switch.style.opacity = "1";
+  setTimeout(function () {
+    Slider_Switch.style.transition = "0s";
+    Slide_To_Unlock_Switch.style.transition = "0s";
+  }, 1000);
+  
+  setTimeout(function () {
+    var Keyboard_Keys = document.getElementsByClassName("Keyboard_Keys");
+    Keyboard_Keys[0].classList.add("Fade_Down_0");
+    Keyboard_Keys[1].classList.add("Fade_Down_1");
+    Keyboard_Keys[2].classList.add("Fade_Down_2");
+    Keyboard_Keys[3].classList.add("Fade_Down_3");
+    Keyboard_Keys[4].classList.add("Fade_Down_4");
+    Keyboard_Keys[5].classList.add("Fade_Down_5");
+    Keyboard_Keys[6].classList.add("Fade_Down_6");
+    Keyboard_Keys[7].classList.add("Fade_Down_7");
+    Keyboard_Keys[8].classList.add("Fade_Down_8");
+    Keyboard_Keys[9].classList.add("Fade_Down_9");
+  }, 50);
+  document.removeEventListener("keydown",Hot_Key_Pressed);
+}
 
 
--- JavaScript/Touch_Screen_Device.js
+++ JavaScript/Touch_Screen_Device.js
@@ -1,8 +1,34 @@
 
 function Detect_Touch_Screen(){  
-  if(window.matchMedia("(pointer: coarse)").matches) {
-    // touchscreen
+  let Is_Touch_Screen = window.matchMedia("(pointer: coarse)").matches;
+  if(Is_Touch_Screen) {
     console.log("Touch screen detected");
+    let Prompt = document.getElementById("Prompt");
+    Prompt.innerHTML = "Tap Letters to Unlock";
   }
-  
+  let Keyboard_Keys = document.getElementsByClassName("Keyboard_Keys");
+  for(Index = 0; Index < Keyboard_Keys.length; Index++){
+    Keyboard_Keys[Index].addEventListener("click",Key_Tapped);
+  }  
 }
+
+function Key_Tapped(){
+  Target_Key = this;
+  Target_Key.classList.add("Key_Toggled");
+  Target_Key.children[0].style.color = "white";
+  Target_Key.children[0].style.backgroundColor = "rgb(70,70,70)";
+  let Keyboard_Keys = document.getElementsByClassName("Keyboard_Keys");
+  Count = 0; 
+  for(Index = 0; Index < Keyboard_Keys.length; Index++){
+    if(Keyboard_Keys[Index].classList.contains("Key_Toggled")){
+      Count = Count + 1
+    }
+  }
+  if(Count == 10){
+    Show_Unlock_Slider();
+  }
+}
+
+
+
+
