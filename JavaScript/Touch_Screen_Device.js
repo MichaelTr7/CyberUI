2de2d72d71922e971bbe155bd613e4daa3519b47
MichaelTr7 <mailmichaeltr@gmail.com>
2021-12-03 10:08:05 -0500

    Touch

--- JavaScript/Touch_Screen_Device.js
+++ JavaScript/Touch_Screen_Device.js
@@ -9,28 +9,10 @@ function Detect_Touch_Screen(){
     // console.log("Not a touch screen");  
   }
   
-  let Keyboard_Keys = document.getElementsByClassName("Keyboard_Keys");
-  for(Index = 0; Index < Keyboard_Keys.length; Index++){
-    Keyboard_Keys[Index].addEventListener("mousedown",Key_Tapped);
-  }  
+
 }
 
-function Key_Tapped(){
-  Target_Key = this;
-  Target_Key.classList.add("Key_Toggled");
-  Target_Key.children[0].style.color = "white";
-  Target_Key.children[0].style.backgroundColor = "rgb(70,70,70)";
-  let Keyboard_Keys = document.getElementsByClassName("Keyboard_Keys");
-  Count = 0; 
-  for(Index = 0; Index < Keyboard_Keys.length; Index++){
-    if(Keyboard_Keys[Index].classList.contains("Key_Toggled")){
-      Count = Count + 1
-    }
-  }
-  if(Count == 10){
-    Show_Unlock_Slider();
-  }
-}
+
 
 
 
